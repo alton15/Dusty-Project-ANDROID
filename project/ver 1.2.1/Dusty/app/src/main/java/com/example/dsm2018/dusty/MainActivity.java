f195681532624ffcf89e1b554e707fd42c8c3ed0
alton15 <alton15@naver.com>
2018-09-19 19:03:02 +0900

    Dusty 프로젝트 ver 1.2.1

---
+++ project/ver 1.2.1/Dusty/app/src/main/java/com/example/dsm2018/dusty/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dsm2018.dusty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
